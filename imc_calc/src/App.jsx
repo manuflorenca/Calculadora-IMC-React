5775573f0749ead89dd3b901cf9109dd0819da1f
ManuFlorenca! <manuelaflorenca22@gmail.com>
2024-12-23 21:13:13 -0300

    aaa

---
+++ imc_calc/src/App.jsx
@@ -0,0 +1,12 @@
+import ImcCalc from "./components/imcCalc";
+import "./imcCalc.css";
+
+function App() {
+  return (
+    <div className="container">
+      <ImcCalc />
+    </div>
+  );
+}
+
+export default App;
---
+++ imc_calc/src/components/imcCalc.jsx
@@ -0,0 +1,22 @@
+const ImcCalc = () => {
+    return (
+      <div id="calc-container">
+        <h2>Calculadora de IMC</h2>
+        <form id="imc-form">
+          <div className="form-inputs">
+            <div className="form-control">
+              <label htmlFor="height">Altura:</label>
+              <input type="text" name="height" id="height" placeholder="Exemplo 1,75" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="weight">Peso:</label>
+              <input type="text" name="weight" id="weight" placeholder="Exemplo 70,5" />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  };
+  
+  export default ImcCalc;
+  
